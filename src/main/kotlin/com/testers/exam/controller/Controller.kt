5ec2762b023c51f4e53b070a3e36c53e3b2c6e18
taiberium <taiberium@mail.ru>
2020-02-07 22:33:42 +0300

    first commit

---
+++ src/main/kotlin/com/testers/exam/controller/Controller.kt
@@ -0,0 +1,23 @@
+package com.testers.exam.controller
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class Controller {
+
+    @PostMapping("/data")
+    fun greeting(@RequestBody input: Input): ResponseEntity<Response> {
+        val goodResponse = ResponseEntity.ok().body(Response("Good request"))
+        val badResponse = ResponseEntity.badRequest().body(Response("Damn BUG Here!"))
+        val containOnlyDigits = input.numbers.chars().allMatch(Character::isDigit)
+
+        return if (containOnlyDigits) goodResponse else badResponse
+    }
+
+}
+
+data class Input(val numbers: String)
+data class Response(val message: String)
